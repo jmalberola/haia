29dd79272eed83b423a85fc6302be2776cc05ca0
Juanmi Alberola <jalberola@dsic.upv.es>
2025-02-18 07:34:40 +0100

    launcher

--- launcher.py
+++ launcher.py
@@ -7,12 +7,13 @@ sagemaker_session = sagemaker.Session()
 # Rol de ejecución (toma el rol de la instancia actual)
 role = sagemaker.get_execution_role()
 
-# Bucket donde subiste genetico.py
-bucket = sagemaker_session.default_bucket()
+# Bucket de S3 donde tengas el genetico
+#bucket = sagemaker_session.default_bucket()
 
 sklearn_estimator = SKLearn(
     entry_point="8reinas.py",  # El script de entrenamiento
     source_dir=".",  # Donde está el código
+    #source_dir="s3://sagemaker-jmi/".format(bucket),  # Esto sería si tienes el código en S3
     role=role,
     instance_type="ml.t3.large",  # Tipo de instancia (puedes cambiarlo) medium no va
     instance_count=1,
@@ -22,5 +23,3 @@ sklearn_estimator = SKLearn(
 )
 
 sklearn_estimator.fit()
-
-#source_dir="s3://sagemaker-jmi/".format(bucket),  # Donde está el código
